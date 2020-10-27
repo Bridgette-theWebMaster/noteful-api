a37ce18d7fbaef9ddf8e930235bebc1c692bdff0
Bridgette <turner.bridgette@yahoo.com>
2020-10-27 12:50:28 -0600

    updates

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,13 @@
-const app = require('../src/app')
+const { expect } = require("chai");
+const supertest = require("supertest");
+const app = require("../src/app");
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+describe("App", () => {
+  it('GET / responds with 200 "Hello, world!"', () => {
+    const token = `bearer ` + process.env.API_TOKEN;
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
-  })
-})
+      .get("/")
+      .set("Authorization", token)
+      .expect(200, "Hello, world!");
+  });
+});
